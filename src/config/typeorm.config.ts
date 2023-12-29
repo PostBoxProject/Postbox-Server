5940ee6d1590cbfa7b391585ca661330b7cdf0ce
DongWon <ehddnjs1747@daum.net>
2023-12-30 00:55:15 +0900

    도커설정

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const typeORMConfig : TypeOrmModuleOptions = {
     type: 'mysql',
-    host: 'localhost',
+    host: 'postbox-db',
     port: 3306,
     username: 'root',
     password: 'ehddnjs369!',
