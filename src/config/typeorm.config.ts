dc3abb28f498acf22088c13373dbedb11b4a9b63
DongWon <ehddnjs1747@daum.net>
2023-12-30 17:46:52 +0900

    .

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,23 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
-export const typeORMConfig : TypeOrmModuleOptions = {
+export const typeORMConfig : TypeOrmModuleOptions = {    
+
+    // type: 'mysql',
+    // host: 'postbox-db',
+    // port: 3306,
+    // username: 'root',
+    // password: 'ehddnjs369!',
+    // database: 'postbox',
+    // entities: [__dirname + '/../**/*.entity.{js,ts}'],
+    // synchronize: true,
+
     type: 'mysql',
     host: 'postbox-db',
     port: 3306,
     username: 'root',
-    password: 'ehddnjs369!',
-    database: 'postbox',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true,
+
 }
