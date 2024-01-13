d88d9be25e97f7b549d5a8c2ca8b1ef8904daa58
DongWon <ehddnjs1747@daum.net>
2024-01-13 16:02:17 +0900

    chore: swagger 설명추가

--- src/letters/letter.controller.ts
+++ src/letters/letter.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Post, Req, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { GetPostBoxId } from "src/decorator/get-postbox.decorator";
 import { CreateLetterRequest } from "./dto/letterRequest";
 import { LetterResponse } from "./dto/letterResponse";
@@ -15,12 +15,14 @@ export class LetterController{
     constructor(private letterService: LetterService){}
 
     @Post()
+    @ApiOperation({summary: 'letter 생성', description: '.'})
     createLetter(@Body() dto: CreateLetterRequest): Promise<LetterResponse> {
         return this.letterService.createLetter(dto);
     }
 
 
     @Get('/postbox')
+    @ApiOperation({summary: '현재 사용자가 받은 letter 조회', description: '.'})
     @UseGuards(AuthGuard())     
     getAllMyLetter(@GetPostBoxId() postboxId: number): Promise<LetterResponse[]>{           
         return this.letterService.getMyLetters(postboxId);
@@ -28,6 +30,7 @@ export class LetterController{
 
 
     @Get('/:id')
+    @ApiOperation({summary: 'letter id로 letter 조회', description: '.'})
     getLetterById(@Param('id') id: number) : Promise<LetterResponse>{
         return this.letterService.getLetterById(id);
     }
@@ -38,11 +41,10 @@ export class LetterController{
         return this.letterService.getAllLetters();
     }
 
-
-    
     
 
     @Delete('/:id')
+    @ApiOperation({summary: 'letter id로 letter 삭제', description: '.'})
     deleteLetter(@Param('id') id: number): Promise<void>{
         return this.letterService.deleteLetter(id);
     }
--- src/postboxs/postboxs.controller.ts
+++ src/postboxs/postboxs.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Param, Post, Query, UseGuards, ValidationPipe } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { AuthRequest } from "./dto/authRequest";
 import { PageRequest } from "./dto/pageRequest";
 import { PostboxRequest } from "./dto/postboxRequest";
@@ -13,21 +13,25 @@ export class PostBoxCotroller{
     constructor(private postboxService: PostBoxService){}
 
     @Post()
+    @ApiOperation({summary: 'postbox 생성', description: '.'})
     createPostBox_temp(@Body() dto: PostboxRequest): Promise<PostboxResponse>{
         return this.postboxService.createPostbox(dto);
     }
 
     @Post('/signin')
+    @ApiOperation({summary: 'postbox 로그인', description: '.'})
     sigIn(@Body() authRequest: AuthRequest){
         return this.postboxService.singIn(authRequest);
     }
 
     @Get()
+    @ApiOperation({summary: '전체 postBox 검색', description: '.'})
     findAllPostBox(@Query() page: PageRequest): Promise<PaginatedPostboxResponse>{
         return this.postboxService.getAllPostbox(page);
     }
 
     @Get('/:keyword')
+    @ApiOperation({summary: 'postBox 키워드로 검색', description: '.'})
     findPostBoxByName(@Query() page: PageRequest, @Param('keyword') keyword: string): Promise<PaginatedPostboxResponse>{
         return this.postboxService.getPostBoxByname(page, keyword);
     }
